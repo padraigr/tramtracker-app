2a7f01645581db4d685eb9f01a1c07ceabc4719f
Padraig Robinson <padraigr91@gmail.com>
2016-12-18 11:51:19 +1100

    Convert to swift 3

--- HomeTime/DotNetDateConverter.swift
+++ HomeTime/DotNetDateConverter.swift
@@ -5,24 +5,24 @@
 import Foundation
 
 
-public class DotNetDateConverter: NSObject {
-    public func dateFromDotNetFormattedDateString(string: NSString) -> NSDate! {
-        let startPositionRange = string.rangeOfString("(")
-        let endPositionRange = string.rangeOfString("+")
+open class DotNetDateConverter: NSObject {
+    open func dateFromDotNetFormattedDateString(_ string: NSString) -> Date! {
+        let startPositionRange = string.range(of: "(")
+        let endPositionRange = string.range(of: "+")
         if startPositionRange.location != NSNotFound && endPositionRange.location != NSNotFound {
             let startLocation = startPositionRange.location + startPositionRange.length
-            let dateAsString = string.substringWithRange(NSRange(location: startLocation, length: endPositionRange.location - startLocation))
+            let dateAsString = string.substring(with: NSRange(location: startLocation, length: endPositionRange.location - startLocation))
             let unixTimeInterval = (dateAsString as NSString).doubleValue / 1000
-            return NSDate(timeIntervalSince1970: unixTimeInterval)
+            return Date(timeIntervalSince1970: unixTimeInterval)
         }
 
         return nil
     }
 
-    public func formattedDateFromString(string: NSString) -> NSString {
+    open func formattedDateFromString(_ string: NSString) -> NSString {
         let date = dateFromDotNetFormattedDateString(string)
-        let formatter = NSDateFormatter()
+        let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm"
-        return formatter.stringFromDate(date)
+        return formatter.string(from: date!) as NSString
     }
 }
